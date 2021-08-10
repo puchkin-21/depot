4c7c3d146448dc4b0e38358d8c98169486ea9dce
Puchkin_21 <mpuchkin@list.ru>
2021-08-10 17:07:43 +0300

    main

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import 'jquery';
+import './application.scss'
 
 Rails.start()
 Turbolinks.start()
